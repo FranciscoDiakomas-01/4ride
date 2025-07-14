dedb7dc013d5f8689082943be3ff0417b140c303
FranDev <franciscodiakoma@gmail.com>
2025-07-14 20:36:05 +0100

    v2

--- src/app/(dashboard)/user/page.tsx
+++ src/app/(dashboard)/user/page.tsx
@@ -8,13 +8,13 @@ import { useRouter } from "next/navigation";
 export default function UserHome() {
   const router = useRouter();
   return (
-    <main className="w-full flex flex-col gap-3 p-4 pt-8">
+    <main className="w-full flex flex-col gap-3 p-4 pt-8 pb-30">
       <div className="flex flex-col">
         <p className="text-md text-gray-500">Um dia incrível pra você</p>
         <h1 className="text-4xl font-semibold text-primary">Francisco!</h1>
       </div>
       <AcountCard amount="100000" number="957777993" />
-      <div className="flex flex-col w-full justify-center items-center">
+      <div className="flex flex-col w-full  justify-center items-center">
         <Image className="h-70 w-80 object-contain" src={car} alt="car" />
         <div className="grid w-full grid-cols-2 gap-3 items-center">
           <Button
