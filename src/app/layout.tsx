cb5e5b37d5bc250cdd493dd74692e7fa8f1139d4
FranDev <franciscodiakoma@gmail.com>
2025-07-14 19:56:49 +0100

    v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ const popins = Poppins({
   variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   display: "swap",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
